4b47473c20c535baf61e3b05946e5a3c0982cb95
“Mateus <devmateussnr@gmail.com>
2023-07-13 12:16:25 -0300

    snake game init

--- day-19-init/main.py
+++ day-19-init/main.py
@@ -1,3 +1,4 @@
+import random
 import turtle
 from turtle import Turtle, Screen
 """
@@ -31,21 +32,37 @@ teus.color("purple")
 teus.penup()
 teus.goto(-230, y=150)
 """
+screen = Screen()
+user_bet = screen.textinput(title="Make your bet", prompt="Wich turtle  will win the race? Enter a color: ")
+screen.setup(width=500, height=400)
 
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions = [-150, -90, -30, 30, 90, 150]
+all_turtles = []
 
 for i in range(0, 6):
-    teus = Turtle(shape="turtle")
-    teus.color(colors[i])
-    teus.penup()
-    teus.goto(-230, y=y_positions[i])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[i])
+    new_turtle.penup()
+    new_turtle.goto(-230, y=y_positions[i])
+    all_turtles.append(new_turtle)
 
-screen = Screen()
-screen.setup(width=500, height=400)
-user_bet = screen.textinput(title="Make your bet", prompt="Wich turtle  will win the race? Enter a color: ")
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_Color = turtle.pencolor()
+            if winning_Color == user_bet:
+                print(f"You've won! The {winning_Color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_Color} turtle is the winner!")
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+
 screen.exitonclick()
-
 """
 def move_forwards():
     teus.forward(10)
