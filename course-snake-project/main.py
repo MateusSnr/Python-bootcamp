074c7c137fc4db076886376d55c10e81ef41b428
“Mateus <devmateussnr@gmail.com>
2023-08-12 17:40:42 -0300

    Game update

--- course-snake-project/main.py
+++ course-snake-project/main.py
@@ -54,10 +54,8 @@ while game_is_on:
         pygame.mixer.music.stop()
 
     #Detect collision with tail
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             pygame.mixer.music.stop()
             score.game_over()
