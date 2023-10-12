d291d06e142c43801fdf6d7204c40f6438800d1b
Mateus Salles <devmateussnr@gmail.com>
2023-10-12 10:22:59 -0300

    Paddle Class
    Adding a paddle class

--- pong_game/venv/main.py
+++ pong_game/venv/main.py
@@ -1,27 +1,24 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
-screen.title("Pong GAME")
+screen.title("Pong Game")
+screen.tracer(0)
 
-paddle =  Turtle()
-paddle.shape("square")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.color("white")
-paddle.penup()
-paddle.goto(350, 0)
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(),new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(),new_y)
+right_paddle = Paddle((350,0))
+left_paddle = Paddle((-350,0))
 
 screen.listen()
-screen.onkey(go_up, "w")
-screen.onkey(go_down, "s")
+screen.onkey(right_paddle.go_up, "w")
+screen.onkey(right_paddle.go_down, "s")
+
+screen.onkey(left_paddle.go_up, "i")
+screen.onkey(left_paddle.go_down, "k")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
